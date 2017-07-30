6ff9e1e3a7539554f1cecbfb40048185cf5fbd79
Clint Priest <github-repo@rxv.me>
2017-07-29 19:47:47 -0500

    Changed build.zsh, learned a bit more about zsh

--- build.zsh
+++ build.zsh
@@ -1,6 +1,7 @@
 #!/bin/env zsh
 
-for dir in `realpath $(dirname **/*.rpm | sort | uniq | perl -pe 's/$/\/../') | sort | uniq`; {
+#for dir in `realpath $(dirname **/*.rpm | sort | uniq | perl -pe 's/$/\/../') | sort | uniq`; {
+for dir in `ls -1d **/*.rpm(:h:h:A) | sort -u`; {
 	echo "Building $dir...";
 	cd $dir;
 	createrepo . | perl -pe 's/^/\t/g';
