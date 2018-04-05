9f3e862c29f18c281f772911c700cfc2b5217c89
Clint Priest <github-repo@rxv.me>
2018-04-05 16:11:48 -0500

    Bugfix on the auto-commit/push of a build.

--- build.zsh
+++ build.zsh
@@ -5,8 +5,10 @@ rpmsign --addsign **/*.rpm |& grep -v 'WARNING: unsafe ownership on configuratio
 
 for dir in `ls -1d **/*.rpm(:h:h:A) | sort -u`; {
 	echo "Building $dir...";
-	cd $dir;
-	createrepo . | perl -pe 's/^/\t/g';
+	( 
+		cd $dir;
+		createrepo . | perl -pe 's/^/\t/g';
+	)
 }
 echo;
 echo "Adding ./CentOS to git and comitting a build release";
