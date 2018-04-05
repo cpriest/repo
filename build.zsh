bcff20ec31e9f1376a9b5b6024ed9b4c4a6a744f
Clint Priest <github-repo@rxv.me>
2018-04-05 14:48:02 -0500

    Update to build.zsh

--- build.zsh
+++ build.zsh
@@ -1,10 +1,11 @@
 #!/bin/env zsh
 
+echo "Signing RPM Packages, enter gpg password...";
+rpmsign --addsign **/*.rpm |& grep -v 'WARNING: unsafe ownership on configuration'
+
 for dir in `ls -1d **/*.rpm(:h:h:A) | sort -u`; {
 	echo "Building $dir...";
 	cd $dir;
 	createrepo . | perl -pe 's/^/\t/g';
 }
 
-echo "Signing RPM Packages, enter gpg passwordi...";
-rpm --addsign **/*.rpm
