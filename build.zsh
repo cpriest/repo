e270c2c8ebf86a1d8f5961aaf5fa462a34b89cfd
Clint Priest <github-repo@rxv.me>
2018-04-06 10:08:45 -0500

    Modified build.zsh to only include CentOS/noarch in the cpriest-noarch repo

--- build.zsh
+++ build.zsh
@@ -7,7 +7,13 @@ for dir in `ls -1d **/*.rpm(:h:h:A) | sort -u`; {
 	echo "Building $dir...";
 	( 
 		cd $dir;
-		createrepo . | perl -pe 's/^/\t/g';
+		if [[ "$dir" == "/opt/repo/repo/CentOS" ]] {
+			ls -1 noarch/**.rpm > noarch.pkgs
+			createrepo --deltas -i noarch.pkgs . | perl -pe 's/^/\t/g';
+			rm -f noarch.pkgs;
+		} else {
+			createrepo --deltas . | perl -pe 's/^/\t/g';
+		}
 	)
 }
 echo;
